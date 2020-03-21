f60a943a6b863f29380c0c82c04359e0b898fe89
jonathandly <dalyj138@gmail.com>
2020-03-21 14:20:34 -0400

    Completed names.py. MVP.

--- names/names.py
+++ names/names.py
@@ -1,5 +1,47 @@
 import time
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert value into tree
+    def insert(self, value):
+        # check if new value is less than current node
+        if value < self.value:
+            # if there is no self.left value
+            if not self.left:
+                # set the new left child to new value
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+
+    def contains(self, target):
+        # if root node equals target return
+        if self.value == target:
+            return True
+        
+        # target is smaller, go left
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -12,11 +54,19 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+binary_search_tree = BinarySearchTree(names_1[0])
+for i in range(1, len(names_1)):
+    binary_search_tree.insert(names_1[i])
+
+for j in range(len(names_2)):
+    if binary_search_tree.contains(names_2[j]):
+        duplicates.append(names_2[j])
+
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
