d5cd6220fdd3df960723447477f0f6068b183210
jonathandly <dalyj138@gmail.com>
2020-03-21 12:35:18 -0400

    Completed ring_buffer, commented out tests for stretch.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,53 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length == 0:
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+        elif self.storage.length == self.capacity and not self.current is self.storage.head:
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+        elif self.storage.length == self.capacity:
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+        else:
+            self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        while self.storage.length > len(list_buffer_contents):
+            list_buffer_contents.append(self.current.value)
+            if self.current.next:
+                self.current = self.current.next
+            else:
+                self.current = self.storage.head
 
         return list_buffer_contents
+
+buffer = RingBuffer(3)
+
+print(buffer.get())   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+print(buffer.get())   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+
+print(buffer.get())  # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+print(buffer.get())   # should return ['d', 'e', 'f']
 
 # ----------------Stretch Goal-------------------
 
